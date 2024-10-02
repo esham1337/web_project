d5121e6da1c308e26c31db6ce11399d0c6796841
esham1337 <158583786+esham1337@users.noreply.github.com>
2024-10-02 07:02:15 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -10,5 +10,14 @@ def index():
 def level1():
     return render_template('level1.html')
 
+@app.route('/level2')
+def level2():
+    return render_template('level2.html')  # This will be the next level page
+@app.route('/level3')
+def level3():
+    return render_template('level3.html')
+@app.route('/level4')
+def level4():
+    return render_template('level4.html')
 if __name__ == '__main__':
     app.run(debug=True)
